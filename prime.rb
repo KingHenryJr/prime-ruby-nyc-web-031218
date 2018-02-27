195c9151d9a7ba50e1dcda8e7cb7635f08489388
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-27 17:05:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(n)
-   if n < 0 || n == 0 || n == 1
-     return false 
-   else
-     (2..n-1).to_a.all? { |factor| num % factor != 0}
-   end
+if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
 end
