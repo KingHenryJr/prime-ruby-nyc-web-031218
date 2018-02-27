78688f256ec6c13e7c61bb3d8d187b850a9ddcce
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-27 16:57:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
-def prime?(num)
- if num % 3 === 1 && num % 2 === 1
-   return true 
-
-
-else return false
-end
+def prime(n)
+  unless n.is_a? Integer
+  puts "That's not an integer."
+  if n%2 !=0 && n!=2
+    puts "#{n} is prime!"
+  else
+    puts "#{n} is not prime."
+  end
 end
